dc5d80d64c53aece003669baba972ee2bfb4a44e
SculpturaFm <mkrtichan2021@gmail.com>
2022-11-16 07:57:27 +0300

    Practice_2

--- js/app.js
+++ js/app.js
@@ -8,7 +8,26 @@ const personalMovieDB = {
   privat: false
 };
 
-const a = prompt('Один из последних просмотренных фильмов', ''),
-      b = prompt('На сколько оцените его ?', '');
 
-personalMovieDB.movies[a] = b;
+for (let i = 0; i < 2; i++) {
+  	const	a = prompt('Один из последних просмотренных фильмов', ''),
+      		b = prompt('На сколько оцените его ?', '');
+
+					if (a != null && b != null && a!= '' && b!= '' && a.length < 50){
+						personalMovieDB.movies[a] = b;
+					} else {
+						i--;
+					}
+}
+
+if (personalMovieDB.count < 10) {
+	console.log('Просмотренно довольно мало фильмов');
+} else if (numberOfFilms >= 10 && numberOfFilms < 30) {
+	console.log('Вы классический зритель');
+} else if (numberOfFilms >= 30) {
+	console.log('Вы киноман');
+} else {
+	console.log('не бывает такого');
+}
+
+console.log(personalMovieDB);
